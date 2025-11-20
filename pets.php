6f11a0ef08426cfe59c431c96a585c22042a0661
diksha <dikshachy1402@gmail.com>
2025-11-20 19:04:47 +0530

    Fix image paths in pets.php
    Updated image paths for stone pets.

--- pets.php
+++ pets.php
@@ -1,10 +1,11 @@
 <?php
 $stone_pets = [
-    ["name" => "Sleepy Steve", "color" => "Gray", "image" => "images/a.jpg", "description" => "Loves naps."],
-    ["name" => "Bouncy Bella", "color" => "Brown", "image" => "images/h.jpg", "description" => "Always energetic."],
-    ["name" => "Chill Charlie", "color" => "White", "image" => "images/o.jpg", "description" => "Calm and serene."],
+    ["name" => "Sleepy Steve", "color" => "Gray", "image" => "a.jpg", "description" => "Loves naps."],
+    ["name" => "Bouncy Bella", "color" => "Brown", "image" => "h.jpg", "description" => "Always energetic."],
+    ["name" => "Chill Charlie", "color" => "White", "image" => "o.jpg", "description" => "Calm and serene."],
 
-    ["name" => "Rocky Ron", "color" => "Dark Gray", "image" => "images/happy.jpg", "description" => "Acts tough but is soft-hearted."],
-    ["name" => "Pebble Penny", "color" => "Beige", "image" => "images/l.jpg", "description" => "Small but full of charm."],
+    ["name" => "Rocky Ron", "color" => "Dark Gray", "image" => "happy.jpg", "description" => "Acts tough but is soft-hearted."],
+    ["name" => "Pebble Penny", "color" => "Beige", "image" => "l.jpg", "description" => "Small but full of charm."],
 ];
 ?>
+
